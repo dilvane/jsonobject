97358a2fdbdbe2d9b6a849b3d452ec6f88e6e297
Aleksandar Yalnazov <yalnazov@gmail.com>
2014-12-17 16:31:41 +0200

    Setup fixed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import sys
 setup(
     name='paymill-jsonobject',
-    version='0.7.0beta',
+    version='0.7.1beta',
     author='Aleksandar Yalnazov',
     author_email='aleksandar.yalnazov@paymill.de',
     description='This is a fork of jsonobject library and is only intended to be used temporary until jsonobject 0.7.0 release is out.',
